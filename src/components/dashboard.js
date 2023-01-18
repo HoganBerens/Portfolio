ba50a93dc711543c78564549deee1c5dab2720f4
Hogan Berens <berenshogan@gmail.com>
2023-01-18 08:24:50 -0600

    pushed changes to dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import AboutMe from './aboutMe';
-import Header from './header';
-import Projects from './projects';
-import './dashboard.css';
+import React from "react";
+import AboutMe from "./aboutMe";
+import Header from "./header";
+import Projects from "./projects";
+import "./dashboard.css";
+
 
 const Dashboard = () => {
   return (
     <div className="wrapper">
-      <div className="header-wrapper">
-        <Header />
-      </div>
-      <div className="children-wrapper">
-        <AboutMe />
-        <Projects />
+      <div className="dashboard-wrapper">
+        <div className="banner-wrapper">
+          <div className="banner-2"></div>
+          <div className="banner-1"></div>
+        </div>
+        <div className="sized-wrapper">
+          <div className="header-wrapper">
+            <Header />
+          </div>
+          <div className="children-wrapper">
+            <AboutMe />
+            <Projects />
+          </div>
+        </div>
+        <div className="banner-wrapper-2">
+          <div className="banner-1"></div>
+          <div className="banner-2"></div>
+        </div>
       </div>
     </div>
   );
