9776873b3fef17e8479cd9baaad1cb17ffeb5744
Hogan Berens <berenshogan@gmail.com>
2023-03-23 14:29:00 -0500

    finished styling

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,7 +5,9 @@ const AboutMe = () => {
   return (
     <div className="aboutMe-wrapper">
       <div className="aboutMe-title">Welcome</div>
-      <div className="aboutMe-content">aboutMe</div>
+      <div className="aboutMe-content">
+        My Name is Hogan Berens, I am a self-taught software developer. 
+      </div>
     </div>
   );
 };
